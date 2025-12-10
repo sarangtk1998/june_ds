b6fe183036925b433aeba7546a7b69418a69b2ce
sarang <sarangtk1998@gmail.com>
2025-12-10 12:20:04 +0530

    msg

--- work.py
+++ work.py
@@ -7,3 +7,22 @@ def add(x,y):
 
 def sub(x,y):
     return x-y
+
+
+
+def add(x,y):
+
+    return x+y
+
+
+def sub(x,y):
+    return x-y
+
+
+def add(x,y):
+
+    return x+y
+
+
+def sub(x,y):
+    return x-y
